a8c99121c77b2b94c9af9169a45188febfc68822
Chris Esplin <christopher.esplin@gmail.com>
2013-06-04 17:41:49 -0600

    Google passport implementation is working.

---
+++ config/convict.js
@@ -0,0 +1,50 @@
+var convict = require('convict'),
+  conf = convict({
+    env: {
+      doc: "The applicaton environment.",
+      format: ["production", "development", "test"],
+      default: "development",
+      env: "NODE_ENV"
+    },
+    port: {
+      doc: "The port to bind.",
+      format: "port",
+      default: 8080,
+      env: "QUIVER_PORT"
+    },
+    sessionSecret: {
+      doc: "Connect session secret",
+      format: "*",
+      default: "You should really change this",
+      env: "QUIVER_SESSION_SECRET"
+    },
+    successRedirect: {
+      doc: "Auth success redirect",
+      format: "*",
+      default: "/",
+      env: "QUIVER_AUTH_SUCCESS_REDIRECT"
+    },
+    failureRedirect: {
+      doc: "Auth failure redirect",
+      format: "*",
+      default: "/login",
+      env: "QUIVER_AUTH_FAILURE_REDIRECT"
+    },
+    google_id: {
+      doc: "Google+ oAuth client ID",
+      format: "*",
+      default: "CHANGE THIS NOW!",
+      env: "QUIVER_AUTH_GOOGLE_CLIENT_ID"
+    },
+    google_secret: {
+      doc: "Google+ oAuth client secret",
+      format: "*",
+      default: "CHANGE THIS NOW!",
+      env: "QUIVER_AUTH_GOOGLE_CLIENT_SECRET"
+    }
+  });
+
+conf.validate();
+
+module.exports = conf;
+
--- quiver-auth.js
+++ quiver-auth.js
@@ -1,1 +1,55 @@
-console.log('using quiver-auth');
+var conf = require('./config/convict.js'),
+  express = require('express'),
+  app = express(),
+  passport = require('passport'),
+  GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: conf.get('sessionSecret') }));
+
+//  Passport Config
+app.use(passport.initialize()); // Initialize Passport!  Also use passport.session() middleware, to support persistent login sessions (recommended).
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: conf.get('google_id'),
+  clientSecret: conf.get('google_secret'),
+  callbackURL: '/quiver-auth/google/return'
+}, function(accessToken, refreshToken, profile, done) {
+  console.log('Google Strategy results', arguments);
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+
+}));
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+//  Passport Routes
+app.get('/quiver-auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+}));
+
+app.get('/quiver-auth/google/return', passport.authenticate('google', { failureRedirect: conf.get('failureRedirect') }), function(req, res) {
+	return res.redirect(conf.get('successRedirect'));
+});
+
+module.exports = app;
+
+
